d778c3a4df40534050dfbf36e0dea2dcdc9c00b2
cmauriciocoronel <cmauriciocoronel@gmail.com>
2020-05-10 20:36:52 -0300

    Se limpia repo de artefactos en desuso.

--- rsk-node-query.js
+++ rsk-node-query.js
@@ -34,3 +34,26 @@ web3.eth.getTransaction(transactionHash).then(transaction => {
     console.log('Transaction: ' + transactionHash);
     console.log(transaction);
 });*/
+
+/*
+const LPFactoryArtifact = require('giveth-liquidpledging/dist/contracts/LPFactory.json');
+const InputDataDecoder = require('ethereum-input-data-decoder');
+const decoder = new InputDataDecoder(LPFactoryArtifact.abiDefinition);
+
+//const data = `0xbce9b995000000000000000000000000cd2a3d9f938e13cd947ec05abc7fe734df8dd826000000000000000000000000cd2a3d9f938e13cd947ec05abc7fe734df8dd826`;
+const data = `0xa2ba31079c60e1d3e08c942e71464be06621a328ca81a1e9fe0c59c2b5e19ba2`;
+const result = decoder.decodeData(data);
+console.log(result);*/
+
+
+var web3 = new Web3("http://localhost:4444");
+//from = '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826';
+/*from = '0x7986b3DF570230288501EEa3nodeD890bd66948C9B79';
+web3.eth.getBalance(from).then(balance => {
+    console.log(`Balance ${from}: ${balance}`);
+});*/
+
+block = '0x8bb5228923c50ecfbf51834c0543ff491d60e07eb172fb251c09b7c51ee7c927';
+web3.eth.getBlock(block).then(b => {
+    console.log(b);
+});
